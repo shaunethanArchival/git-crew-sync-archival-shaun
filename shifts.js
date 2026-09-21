33881a35cff06a5e69a0679f6c29b08e8a0cd6f6
Shaun Archival <shaunethanarchival@gmail.com>
2026-09-21 12:52:37 +0800

    :wqAdd minimum pay floor - archival.shaun

--- shifts.js
+++ shifts.js
@@ -3,13 +3,16 @@ function isValidShift(hours) {
 }
 
 function calculatePay(hours, rate) {
+  let pay;
   if (hours > 8) {
     const regularPay = 8 * rate;
     const overtimeHours = hours - 8;
     const overtimePay = overtimeHours * rate * 1.5;
-    return Math.round(regularPay + overtimePay);
+    pay = Math.round(regularPay + overtimePay);
+  } else {
+    pay = Math.round(hours * rate);
   }
-  return Math.round(hours * rate);
+  return Math.max(pay, 15);
 }
 
 module.exports = { isValidShift, calculatePay };
